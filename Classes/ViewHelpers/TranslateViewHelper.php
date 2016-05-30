4117be30e65a82b09aa34b8543fdea38fc042425
Ruben Schmidmeister <ruben@hausformat.com>
2016-05-30 11:59:53 +0200

    update TranslateViewHelper for typo3 7.6.x

--- Classes/ViewHelpers/TranslateViewHelper.php
+++ Classes/ViewHelpers/TranslateViewHelper.php
@@ -1,6 +1,10 @@
 <?php
 namespace Roquin\RoqNewsevent\ViewHelpers;
 
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Exception\InvalidVariableException;
+
 /**
  * Copyright (c) 2012, ROQUIN B.V. (C), http://www.roquin.nl
  *
@@ -10,21 +14,23 @@ namespace Roquin\RoqNewsevent\ViewHelpers;
  */
 class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelper
 {
-
     /**
-     * Translate a given key or use the tag body as default.
+     * Return array element by key.
      *
-     * @return string The translated key or tag body if key doesn't exist
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @throws InvalidVariableException
+     * @return string
      */
-    public function render()
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
     {
-        $value = parent::render();
+        $value = parent::renderStatic($arguments, $renderChildrenClosure, $renderingContext);
 
-        if (!isset($value)) {
-            $value = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate($this->arguments['key'], 'roq_newsevent',
-                $this->arguments);
+        if (isset($value)) {
+            return $value;
         }
 
-        return $value;
+        return LocalizationUtility::translate($arguments['key'], 'roq_newsevent', $arguments);
     }
 }
